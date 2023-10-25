9f819cdbff209387ffeb2053a70ae0e823a4e0b5
Alexsandra <alexsandra.bonfim.vieira@gmail.com>
2023-10-25 15:27:50 -0300

    update-home

--- template-home.php
+++ template-home.php
@@ -8,17 +8,28 @@ $posts_receita = get_posts();
 //var_dump($posts_receita);
 
 ?>
+    <div class="container">
+        <div class="row">
+           
 
-    <h1>Home</h1>
+                <h1>Humm... Delícias Fáceis de Fazer!</h1>
+                <?php
+                    foreach($posts_receita as $receita):
+                ?>
+                    <div class="col-md-6">
+                        <a href="<?= get_permalink($receita); ?>">
+                        <h2><?php echo $receita->post_title; ?></h2>
+                        <img class="img-fluid" src="<?= get_the_post_thumbnail_url($receita); ?>">
+                        </a>
+                    </div>   
+                <?php        
+                    endforeach;
+                ?>
+            
+        </div>    
+    </div>
+
     <?php
-        foreach($posts_receita as $receita):
-    ?>
-        <a href="<?= get_permalink($receita); ?>">
-        <h2><?php echo $receita->post_title; ?></h2>
-        <img src="<?= get_the_post_thumbnail_url($receita); ?>">
-        </a>
-    <?php        
-        endforeach;
 
 
         get_footer();
