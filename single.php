36e6dcd8fb5ab10b27cc73d878d1ca6b4916180d
Alexsandra <alexsandra.bonfim.vieira@gmail.com>
2023-10-25 12:51:51 -0300

    create template da home

--- single.php
+++ single.php
@@ -1,15 +1,10 @@
 <?php
+
+    get_header();
     $titulo = get_the_title();
     $titulo = mb_strtoupper($titulo);
 ?>
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Meu primeiro código PHP</title>
-</head>
-<body>
+
 
     
     <h1><?= $titulo; ?></h1>
@@ -23,5 +18,6 @@
     </ol>
 
     
-</body>
-</html>
+<?php 
+    get_footer();
+?>
