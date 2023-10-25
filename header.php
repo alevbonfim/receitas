6fd6797a54e2256cb7437a85c96753baea27e657
Alexsandra <alexsandra.bonfim.vieira@gmail.com>
2023-10-25 15:22:13 -0300

    create-menu

--- header.php
+++ header.php
@@ -3,9 +3,45 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Meu primeiro código PHP</title>
+    <title>Receitas da Alê</title>
     <link rel="stylesheet" href="<?= get_template_directory_uri(); ?>/node_modules/bootstrap/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="<?= get_template_directory_uri(); ?>/style.css">
 </head>
 <body>
 
-menu
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container">
+    <a class="navbar-brand logo-menu" href="<?php echo get_site_url() ?>/">Receitas da Alê</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="<?php echo get_site_url(); ?>/">Home</a>
+        </li>
+        
+        
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Receitas
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Pratos Salgados</a></li>
+            <li><a class="dropdown-item" href="#">Pratos Doces</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="#">Outros...</a></li>
+          </ul>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Contato</a>
+        </li>
+        
+      </ul>
+      <form class="d-flex" role="search">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
