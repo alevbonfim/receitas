420ffeee544c2eb404edd4cdd33c5e9cf8ab7d9f
Alexsandra <alexsandra.bonfim.vieira@gmail.com>
2023-10-25 12:51:00 -0300

    create header

---
+++ header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Meu primeiro código PHP</title>
+    <link rel="stylesheet" href="<?= get_template_directory_uri(); ?>/node_modules/bootstrap/dist/css/bootstrap.min.css">
+</head>
+<body>
+
+menu
